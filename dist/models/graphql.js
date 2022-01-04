0654ddd4b87f6dc365bd21358fd23d5ed416bfa2
Hendrickson <Paul.Hendrickson@biworldwide.com>
2022-01-04 11:10:18 -0600

    deleted dist

--- dist/models/graphql.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MissionFlag = void 0;
-var MissionFlag;
-(function (MissionFlag) {
-    MissionFlag["Featured"] = "FEATURED";
-    MissionFlag["InProgress"] = "IN_PROGRESS";
-})(MissionFlag = exports.MissionFlag || (exports.MissionFlag = {}));
--- dist/schema/resolvers/externalUrl.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.externalUrlResolve = void 0;
-exports.externalUrlResolve = {
-    displayText: () => "google",
-    href: () => "https://www.google.com/",
-};
